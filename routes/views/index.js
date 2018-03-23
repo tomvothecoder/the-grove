d60e6d65a814c179452feef089da5c4584e69ceb
Tom Vo <t_vo16@u.pacific.edu>
2018-03-22 17:55:32 -0700

    Merge branch 'master' into origin/master
    Integrated socket.io successfully with keystone.js!

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,7 +8,7 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-
+	req.session.message = 'Hello World';
 	// Render the view
 	view.render('index');
 };
